18069ead8f5638d3c7f63e5bb1d69e5c371cdcf7
messi <15904012203@163.com>
2021-02-16 11:12:17 +0800

    更新了master分支，更新hello.java3

--- HelloGit/src/main/java/com/example/git/Hello.java
+++ HelloGit/src/main/java/com/example/git/Hello.java
@@ -3,6 +3,7 @@ package com.example.git;
 public class Hello {
 
     public static void main(String[] args) {
+        System.out.println("hello git！更新了master分支");
         System.out.println("hello git!");
     }
 }
